0f0dbbec292cc1a839642da4205a2a39fa8364f5
Patel <HPatel@unitedwater.com>
2017-12-11 15:07:08 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+
+            Console.WriteLine("\nWhat is your name ? (Creepy)");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine("\nHello {0}, on {1:d} at {1:t}", name, date);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
